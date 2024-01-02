7c20714323e6267fcc2dc559fca9ec43f9d50956
ketan <ketan.akbari123@gmail.com>
2024-01-02 18:02:12 +0530

    added prettier

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,54 @@
 import { Component } from '@angular/core';
 import { Router, NavigationCancel, NavigationEnd } from '@angular/router';
-import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import {
+  Location,
+  LocationStrategy,
+  PathLocationStrategy,
+} from '@angular/common';
 import { filter } from 'rxjs/operators';
 import { AdminService } from './services/admin.service';
 declare let $: any;
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    providers: [
-        Location, {
-            provide: LocationStrategy,
-            useClass: PathLocationStrategy
-        }
-    ]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  providers: [
+    Location,
+    {
+      provide: LocationStrategy,
+      useClass: PathLocationStrategy,
+    },
+  ],
 })
 export class AppComponent {
-    location: any;
-    routerSubscription: any;
+  location: any;
+  routerSubscription: any;
 
-    constructor(
-        private router: Router,
-        private adminService:AdminService
-    ) {
+  constructor(private router: Router, private adminService: AdminService) {}
 
-    }
+  ngOnInit() {
+    this.recallJsFuntions();
+  }
 
-    ngOnInit(){
-        this.recallJsFuntions();
-    }
+  recallJsFuntions() {
+    this.routerSubscription = this.router.events
+      .pipe(
+        filter(
+          (event) =>
+            event instanceof NavigationEnd || event instanceof NavigationCancel
+        )
+      )
+      .subscribe((event) => {
+        this.location = this.router.url;
+        if (!(event instanceof NavigationEnd)) {
+          return;
+        }
+        window.scrollTo(0, 0);
+      });
+  }
 
-    recallJsFuntions() {
-        this.routerSubscription = this.router.events
-        .pipe(filter(event => event instanceof NavigationEnd || event instanceof NavigationCancel))
-        .subscribe(event => {
-            this.location = this.router.url;
-            if (!(event instanceof NavigationEnd)) {
-                return;
-            }
-            window.scrollTo(0, 0);
-        });
-    }
-
-    isAdmin(): boolean {
-      return this.adminService.isAdminUser();
-    }
+  isAdmin(): boolean {
+    return this.adminService.isAdminUser();
+  }
 }
